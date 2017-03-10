57442cd2a2b42963130ae52c961abcca34b075b6
Flying cat <fredx86@users.noreply.github.com>
2017-03-10 19:54:43 +0100

    Update barray.h

--- bytearray/barray.h
+++ bytearray/barray.h
@@ -28,6 +28,6 @@ void* ba_find_byte(const ba_t*, char);
 void ba_destroy(ba_t*);
 
 int ba_memory_for(ba_t*, size_t n); /* Allocate enough memory for size n + actual size of array. Return 0 on error, 1 on success */
-size_t ba_alloc_size(size_t n); /* Return in bytes, the alloc size needed from bytes of size n */
+size_t ba_alloc_size(size_t n); /* Return in bytes, the alloc size needed for n bytes */
 
 #endif
