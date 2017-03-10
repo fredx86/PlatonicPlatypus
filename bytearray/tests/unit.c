322e523191a4c70c2ad4115f24f852ddae99658d
Flying cat <fredx86@users.noreply.github.com>
2017-03-10 20:11:22 +0100

    Update unit.c

--- bytearray/tests/unit.c
+++ bytearray/tests/unit.c
@@ -12,8 +12,11 @@ int main()
   assert(ba_app(NULL, "a", 1) == NULL);
   assert(ba_app(array, "a", 1) != NULL);
   assert(array->size == 1);
+  assert(array->bytes[0] == 'a');
   assert(ba_app(array, "bc", 2) != NULL);
   assert(array->size == 3);
+  assert(array->bytes[1] == 'b');
+  assert(array->bytes[2] == 'c');
   assert(ba_app_byte(array, 'd') != NULL);
   assert(array->size == 4);
   assert(ba_app(array, big_alloc, 4096) != NULL);
