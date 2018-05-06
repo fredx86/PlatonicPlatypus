de80cd32eec529ab9f9e5167e7463172079ab62b
Theo <ardoui.t@gmail.com>
2018-05-06 16:29:59 +0200

    Fix regtest strset()

--- tests/utils_strset.c
+++ tests/utils_strset.c
@@ -6,8 +6,8 @@
 int main()
 {
   char buf[8];
-  assert(strcmp(strset(buf, 8, "test"), "test") == 0);
-  assert(strcmp(strset(buf, 2, "test"), "t") == 0);
-  assert(strcmp(strset(buf, 1, "test"), "") == 0);
+  assert(strcmp(strset(buf, "test", 8), "test") == 0);
+  assert(strcmp(strset(buf, "test", 2), "t") == 0);
+  assert(strcmp(strset(buf, "test", 1), "") == 0);
   return (0);
 }
