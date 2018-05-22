95e3ae825e21128397b35c208de0627fbf05124f
Theo <ardoui.t@gmail.com>
2018-05-23 00:10:39 +0200

    Logger improvements

--- includes/pp/logger.h
+++ includes/pp/logger.h
@@ -20,18 +20,29 @@ struct log_info
   const char* function;
 };
 
-extern FILE* _logger_stream;
-
-#define TRC_PACKED(id)  (&(struct log_info) {id, __LINE__, __FILE__, __func__})
-#define TRC_INFO        TRC_PACKED(LOGGER_INFO)
-#define TRC_WARN        TRC_PACKED(LOGGER_WARN)
-#define TRC_ERR         TRC_PACKED(LOGGER_ERR)
-#define TRC_FATAL       TRC_PACKED(LOGGER_FATAL)
+struct log_cfg
+{
+  const char* time_format;
+  FILE* stream;
+};
 
 /*
-  @description  Log a message to the _logger_stream
+  Logger's configuration
+  If stream is NULL, will use stderr
+  If time_format is NULL, will use '%d-%m-%y %X' format (uses strftime)
+*/
+extern struct log_cfg _logger_cfg;
+
+#define _TRC_PACKED(id)  (&(struct log_info) {id, __LINE__, __FILE__, __func__})
+#define TRC_INFO        _TRC_PACKED(LOGGER_INFO)
+#define TRC_WARN        _TRC_PACKED(LOGGER_WARN)
+#define TRC_ERR         _TRC_PACKED(LOGGER_ERR)
+#define TRC_FATAL       _TRC_PACKED(LOGGER_FATAL)
+
+/*
+  @description  Log a message using the _logger_cfg
+                info parameter should be created by using a TRC_* macro
                 Use internally vfprintf()
-                If _logger_stream is NULL, will log on stderr
 */
 void logger(const struct log_info* info, const char* format, ...);
 
--- srcs/logger.c
+++ srcs/logger.c
@@ -2,18 +2,34 @@
 #include <time.h>
 #include <stdio.h>
 
-FILE* _logger_stream = NULL;
+struct log_cfg _logger_cfg = {
+  .time_format = NULL,
+  .stream = NULL
+};
 
 static const char* _log_str[] = {
   "INFO",
   "WARN",
-  "ERR",
+  "ERROR",
   "FATAL"
 };
 
 static inline FILE* get_stream()
 {
-  return _logger_stream ? _logger_stream : stderr;
+  if (!_logger_cfg.stream)
+  {
+    return stderr;
+  }
+  return _logger_cfg.stream;
+}
+
+static inline const char* get_time_format()
+{
+  if (!_logger_cfg.time_format)
+  {
+    return "%d-%m-%y %X";
+  }
+  return _logger_cfg.time_format;
 }
 
 void logger(const struct log_info* info, const char* format, ...)
@@ -23,10 +39,10 @@ void logger(const struct log_info* info, const char* format, ...)
   time_t now = time(NULL);
 
   va_start(ap, format);
-  strftime(buf, 32, "%d/%m/%y %X", localtime(&now));
-  fprintf(get_stream(), "[%s] [ %s ] <%s:%u(%s)> ",
+  strftime(buf, 32, get_time_format(), localtime(&now));
+  fprintf(get_stream(), "%s [ %s ] <%s:%u:%s> ",
     buf, _log_str[info->id], info->file, info->line, info->function);
-  vfprintf(stderr, format, ap);
-  fprintf(stderr, "\n");
-  va_end (ap);
+  vfprintf(get_stream(), format, ap);
+  fprintf(get_stream(), "\n");
+  va_end(ap);
 }
